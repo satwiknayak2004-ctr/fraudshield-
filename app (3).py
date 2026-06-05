070b917e4f00090aac3a2d0a334538d886580241
satwiknayak2004-ctr <satwiknayak2004@gmail.com>
2026-06-06 01:24:28 +0530

    Update app (3).py

--- app (3).py
+++ app (3).py
@@ -13,7 +13,7 @@ st.set_page_config(page_title='FraudShield', page_icon='🛡️', layout='wide')
 @st.cache_resource
 def load_models():
     xgb = pickle.load(open('fraud_model_xgb.pkl', 'rb'))
-    lgbm = pickle.load(open('fraud_model_lgbm (1).pkl', 'rb'))
+    lgbm = pickle.load(open('fraud_model_lgbm .pkl', 'rb'))
     rf = pickle.load(open('fraud_model_rf.pkl', 'rb'))
     cfg = pickle.load(open('ensemble_config.pkl', 'rb'))
     explainer = shap.TreeExplainer(xgb)
